73d356734d44a090c6d2757a0fba6176d840afad
MatusZ <matus.zahorec2@gmail.com>
2024-06-08 22:09:33 +0200

    upravena vertia

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,50 @@
 import { Button } from "@/components/ui/button"
 import { ToastAction } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
+import { useState } from "react"
 
 export default function Home() {
 
   const { toast } = useToast()
+
+  const [handler , setHandler] = useState(0)
+
+  const pripocitaj = () => {
+    setHandler(handler+1)
+  }
+
+  const odcitaj = () => {
+    setHandler(handler-1)
+  }
+
+  const resetuj = () => {
+    setHandler(0)
+  }
+
+
+
   
 
   return (
-    <div>
+    <div className="flex justify-center">
+      <button onClick={odcitaj} className="pt-2 pb-2 bg-teal-500 pl-8 pr-8 text-white ">{handler}</button>
+      <h1>klikni na cislo a odcitaj ho</h1>
+
+
         <Button
-      variant="secondary"
+      variant="destructive"
       className=""
       onClick={() => {
         toast({
-          title: "Oops nastala chyba.",
-          description: "Skúste to znova prosím.",
-          action: <ToastAction altText="Try again">Skúsiť znovu</ToastAction>,
+          className: "mb-1",
+          variant: "destructive",
+          title: "Oops citim klik.",
+          description: "klikol si",
+          action: <ToastAction onClick={resetuj} altText="Try again">Resetovať</ToastAction>,
         })
       }}
     >
-      Zobraziť chybu
+      Klikni a zobraz upozornenie
     </Button>
 
     </div>
